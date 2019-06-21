887e5e88ef256f1de53037968de94360890027c9
agaidt <agaidt@lineate.com>
2019-06-21 11:43:38 +0600

    first commit

---
+++ src/main/java/com/trainee/ansible/AnsibleApplication.java
@@ -0,0 +1,13 @@
+package com.trainee.ansible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainee/ansible/AnsibleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trainee.ansible;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnsibleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
